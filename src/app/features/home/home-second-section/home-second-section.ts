faa47b2ebb80a308ab021f1d868768493d7ed690
valiyeffa <firuzeveliyeva2@gmail.com>
2026-04-12 18:53:13 +0400

    home second section done

--- src/app/features/home/home-second-section/home-second-section.ts
+++ src/app/features/home/home-second-section/home-second-section.ts
@@ -8,32 +8,38 @@ import { ICONS } from '../../../constants/icons';
   styleUrl: './home-second-section.scss',
 })
 export class HomeSecondSection {
-  data =[
+  data = [
     {
-      img:ICONS.homePageImages.secondSectionIcons.libraryIcon,
-      title:'Kitabxana',
+      img: ICONS.homePageImages.secondSectionIcons.libraryIcon,
+      title: 'Kitabxana',
+      circle: true
     },
     {
-      img:ICONS.homePageImages.secondSectionIcons.carierIcon,
-      title:'Karyera və məşğulluq',
+      img: ICONS.homePageImages.secondSectionIcons.carierIcon,
+      title: 'Karyera və məşğulluq',
+      circle: false
     },
     {
-      img:ICONS.homePageImages.secondSectionIcons.examIcon,
-      title:'Onlayn imtahan',
+      img: ICONS.homePageImages.secondSectionIcons.examIcon,
+      title: 'Onlayn imtahan',
+      circle: true
     },
     {
-      img:ICONS.homePageImages.secondSectionIcons.apelyasiyaIcon,
-      title:'Onlayn appellyasiya',
+      img: ICONS.homePageImages.secondSectionIcons.apelyasiyaIcon,
+      title: 'Onlayn appellyasiya',
+      circle: false
     },
     {
-      img:ICONS.homePageImages.secondSectionIcons.metodicPersonIcon,
-      title:'Elmi - Metodiki Şura',
+      img: ICONS.homePageImages.secondSectionIcons.metodicPersonIcon,
+      title: 'Elmi - Metodiki Şura',
+      circle: true
     },
     {
-      img:ICONS.homePageImages.secondSectionIcons.volunterIcon,
-      title:'Könüllü',
+      img: ICONS.homePageImages.secondSectionIcons.volunterIcon,
+      title: 'Könüllü',
+      circle: false
     },
   ];
 
-  
+
 }
